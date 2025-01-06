acbd3223c029d906d1112205d5bd80dda42c2325
Ambre Ghisalberti <ambre.ghisalberti@lpp.polytechnique.fr>
2025-01-06 16:18:08 +0100

    almost nothing, just a test for versions

--- swapp/windowing/gradient_boosting/__init__.py
+++ swapp/windowing/gradient_boosting/__init__.py
@@ -1,10 +1,10 @@
 from sklearn.preprocessing import StandardScaler
-from sklearn.impute import SimpleImputer
 import pandas as pd
 import numpy as np
 import os
 from scipy.signal import medfilt
 __HERE__ = os.path.dirname(__file__)
+
 
 def standardScaling(df):
     """ Precondition : df has to be very big, so the scaling is close to the one of the traintest of the model.
