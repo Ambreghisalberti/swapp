315e7ec28516ca827c2f4e1019f484de0d50c201
Ambre Ghisalberti <ambre.ghisalberti@lpp.polytechnique.fr>
2025-04-07 16:29:24 +0200

    db

--- swapp/make_plots/plot_maps.py
+++ swapp/make_plots/plot_maps.py
@@ -511,7 +511,7 @@ def plot_maps(interpolated_features, **kwargs):
         fig, ax[i // ncols, i % ncols] = planet_env.layout_earth_env(msh, figure=fig,
                                                                      axes=np.array([ax[i // ncols, i % ncols]]),
                                                                      y_lim=(-17, 17), z_lim=(-15, 15), x_slice=0)
-    for a in ax.flatten():
+    for a in ax.ravel():
         a.set_aspect('equal')
     fig.tight_layout()
     return fig, ax
